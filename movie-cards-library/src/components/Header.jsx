3f2ee663a69717cc9c4e83774d520931528cc07f
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-09-27 15:11:12 -0300

    Requisitos 1 e 2 - Feito

--- movie-cards-library/src/components/Header.jsx
+++ movie-cards-library/src/components/Header.jsx
@@ -4,7 +4,15 @@ class Header extends React.Component {
   render() {
     
     return (
-      
+      <div>
+        <header>
+          <h1>
+            Movie Cards Library
+          </h1>
+        </header>
+      </div>
     )
   }
 }
+
+export default Header;
